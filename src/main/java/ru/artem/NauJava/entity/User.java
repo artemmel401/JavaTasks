efdc20f343761ed2eb6e264b3f0680727965f0db
artemmel401 <125571136+artemmel401@users.noreply.github.com>
2026-05-19 13:40:56 +0500

    some fixes

--- src/main/java/ru/artem/NauJava/entity/User.java
+++ src/main/java/ru/artem/NauJava/entity/User.java
@@ -1,5 +1,7 @@
 package ru.artem.NauJava.entity;
 
+import com.fasterxml.jackson.annotation.JsonAlias;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
@@ -22,15 +24,23 @@ public class User {
     private String email;
 
     @Column(name = "first_name")
+    @JsonProperty("first_name")
+    @JsonAlias("firstName")
     private String firstName;
 
     @Column(name = "last_name")
+    @JsonProperty("last_name")
+    @JsonAlias("lastName")
     private String lastName;
 
     @Column(name = "is_active")
+    @JsonProperty("is_active")
+    @JsonAlias("active")
     private boolean isActive;
 
     @Column(name = "registration_date")
+    @JsonProperty("registration_date")
+    @JsonAlias("registrationDate")
     private LocalDateTime registrationDate;
 
     @Enumerated(EnumType.STRING)
